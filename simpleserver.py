c355612d92e7ceae1752e0a35c5885093cafd431
k0h4sh1 <k.0h4sh1@gmail.com>
2018-08-25 21:10:47 +0900

    Delete scripts.

--- simpleserver.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# coding: utf-8
-import http.server
-
-server_address = ("", 8000)
-handler_class = http.server.SimpleHTTPRequestHandler
-simple_server = http.server.HTTPServer(server_address, handler_class)
-simple_server.serve_forever()
